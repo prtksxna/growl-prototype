5e556cf8c576c3ab939792ebbce1c49e3a9bb10f
Prateek Saxena <prtksxna@gmail.com>
2008-10-16 23:05:46 +0530

    Fixed IE PNG problem, optimized JS

--- js/growl.js
+++ js/growl.js
@@ -5,16 +5,16 @@ var Growl = Class.create({
 		this.keeper  = new Element('div', {'class': "growl-keeper", 'id': 'growl_keeper' }); //Create a container where 
 		document.body.appendChild(this.keeper);
 	},
-	display: function(text,opt){
+	display: function(text, opt){
 		this.n++;
-		var delay = 5000;
-		var image = "asterisk_orange.png";
-		if (opt && opt.delay) delay = opt.delay*1000;
-		if (opt && opt.image)  image = opt.image;
-		var html = "<img src='"+this.imageFolder+""+image+"' alt='' class='growl-icon' />" + "<p class='growl-text'>"+text.capitalize()+"</p>";
-		var growlAlert = new Element('div', {'class': "growl-alert",	'id': "growl-alert-"+this.n}).update(html).setOpacity(0).appear({to: 0.8}).observe('click',function() {new Effect.DropOut(this.id);});
-		this.keeper.appendChild(growlAlert);
-		timer = setTimeout( "new Effect.DropOut('growl-alert-"+this.n+"')", delay);
-		return $("growl-alert-"+this.n);
+		var delay = 5, gsrc = this.imageFolder+"asterisk_orange.png", gid = "growl-alert-"+this.n;
+		if (opt && opt.delay) delay = opt.delay;
+		if (opt && opt.image) gsrc = this.imageFolder+""+opt.image;
+		this.keeper.insert({bottom: new Element('div', {'class': "growl-alert",	'id': gid})});
+		$(gid).insert({top: new Element('img',{'class':'growl-icon', 'src': gsrc}), bottom: new Element('p', {'class': 'growl-text'}).update(text.capitalize())}).setOpacity(0).appear({to: 0.8}).observe('click',function() {
+			new Effect.DropOut(this.id);
+		}).setPngBackground(image,{ backgroundColor: '#000000'});
+		Effect.DropOut.delay(delay, gid)
+		return $(gid);
 	}
 });
