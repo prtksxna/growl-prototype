1b343aefb32cefec17fe9aaa0792de70226eaafe
Prateek Saxena <prtksxna@gmail.com>
2008-12-30 23:03:17 +0530

    The update doesn't dissapear if the mouse pointer is over it

--- js/growl.js
+++ js/growl.js
@@ -11,8 +11,15 @@ var Growl = Class.create({
     var gid = "growl-alert-"+this.n, opt = Object.extend({image: "asterisk_orange.png",delay: 5, sound: false, }, arguments[1] || {});
 		if(this.sound || opt.sound) Sound.play(this.mediaFolder+"sounds/waterdrop.mp3");
     this.keeper.insert({bottom: new Element('div', {'class': "growl-alert",  'id': gid})});
-    $(gid).insert({top: new Element('img',{'class':'growl-icon', 'src': this.mediaFolder+"icons/"+opt.image}), bottom: new Element('p', {'class': 'growl-text'}).update(text.capitalize())}).setOpacity(0).appear({to: 0.8}).observe('click',function() {new Effect.DropOut(this.id)}).setPngBackground(opt.image,{ backgroundColor: '#000000'});
-    Effect.DropOut.delay(opt.delay, gid);
+		var me = this;
+    $(gid).insert({top: new Element('img',{'class':'growl-icon', 'src': this.mediaFolder+"icons/"+opt.image}), bottom: new Element('p', {'class': 'growl-text'}).update(text.capitalize())}).setOpacity(0).appear({to: 0.8}).observe('click',function(e) {me.remove(Event.findElement(e,'DIV').id);}).setPngBackground(opt.image,{ backgroundColor: '#000000'});
+		$(gid).observe('mouseover',function(e) {this.addClassName('growl-alert-hover')});
+		$(gid).observe('mouseout',function(e) {this.removeClassName('growl-alert-hover')});
+    this.remove.delay(opt.delay, gid);
 		return $(gid);
-  }
+  },
+	remove: function(gid){
+		if(!$(gid).hasClassName('growl-alert-hover')) Effect.DropOut(gid);
+		else this.delay(1,gid);
+	},
 });
